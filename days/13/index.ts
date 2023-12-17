9c320a8d03711bab6c77f8865218ccb7d0ce7462
Sam Dale <sam@romeodigital.com.au>
2023-12-17 12:26:11 +1100

    Day 13 part 2 solution

--- days/13/index.ts
+++ days/13/index.ts
@@ -91,4 +91,64 @@ await answer(1, (input) => {
  * Part 2
  */
 
-await answer(2, (input) => {})
+function countSmudges(a: number, b: number): number {
+	let smudges = 0
+	const diffs = (a ^ b).toString(2)
+	for (let i = 0; i < diffs.length; i++) {
+		if (diffs[i] === '1') smudges++
+	}
+	return smudges
+}
+
+function matchesWithSmudge(a: number, b: number): boolean {
+	return countSmudges(a, b) === 1
+}
+
+function findReflectionIndexWithSmudge(input: number[]): number {
+	for (let i = 1; i < input.length; i++) {
+		const left = reverse(input.slice(0, i))
+		const right = input.slice(i)
+
+		// Assume a smudge could be in any position
+		const minLength = Math.min(left.length, right.length)
+		for (let maybeSmudgeAt = 0; maybeSmudgeAt < minLength; maybeSmudgeAt++) {
+			let isReflection = true
+			for (let j = 0; j < minLength; j++) {
+				// Check this position for a reflection (or a smudged reflection)
+				if (j === maybeSmudgeAt) {
+					if (!matchesWithSmudge(left[j], right[j])) {
+						isReflection = false
+						break
+					}
+				} else {
+					if (left[j] !== right[j]) {
+						isReflection = false
+						break
+					}
+				}
+			}
+			if (isReflection) {
+				return i
+			}
+		}
+	}
+	return -1
+}
+
+function findReflectionWithSmudge(mirror: Mirror): Reflection {
+	const row = findReflectionIndexWithSmudge(mirror.rows)
+	if (row > -1) return { row }
+	const col = findReflectionIndexWithSmudge(mirror.cols)
+	return { col }
+}
+
+function summariseMirrorWithSmudge(mirror: Mirror): number {
+	const reflection = findReflectionWithSmudge(mirror)
+	if ('row' in reflection) return reflection.row * 100
+	return reflection.col
+}
+
+await answer(2, (input) => {
+	const mirrors = groupInput(input).map(parseMirror)
+	return sum(mirrors.map(summariseMirrorWithSmudge))
+})
